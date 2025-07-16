797d5367b26f1b603a9d860d9343cfb0fdb927b2
Mohammed Sajad VT <Sajad.vt@kiebot.com>
2025-07-16 19:37:33 +0530

    Added z-index

--- src/components/DeviceCard.jsx
+++ src/components/DeviceCard.jsx
@@ -41,7 +41,7 @@ function DeviceCard() {
         <div className='grid md:grid-cols-3   pt-10 gap-5'>
             {
                 deviceData.map((data) => (
-                    <div className='bg-gradient-to-tr sm:mt-2 cursor-pointer transition hover:-translate-y-1 ease-in-out delay-150 duration-300  from-[#0F0F0F] min-w-[350px] to-[#E50000]/15  via-[#1A1A1A] px-8 py-8  rounded-lg'>
+                    <div className='bg-gradient-to-tr sm:mt-2 cursor-pointer transition-transform hover:-translate-y-1 ease-in-out delay-150 duration-300  from-[#0F0F0F] min-w-[350px] to-[#E50000]/15  via-[#1A1A1A] px-8 py-8  rounded-lg'>
                         <div className='flex gap-3 items-center'>
                             <div className='border  w-[50px] h-[50px]  bg-[#141414]  border-[#1F1F1F] rounded-md flex justify-center items-center'>
                                 <img src={data.image} alt={data.deviceName} className='w-[20px]' />
--- src/components/GenreCard.jsx
+++ src/components/GenreCard.jsx
@@ -4,7 +4,7 @@ import { imageUrl } from '../constants/api';
 import { Skeleton } from '../components/ui/skeleton';
 function GenreCard({ genreName, movies = [] ,loading}) {
   return (
-    <div className="bg-[#1A1A1A] cursor-pointer transition hover:-translate-y-1 ease-in-out delay-150 duration-300  rounded-xl px-4 min-w-[170px] flex-shrink-0 w-[170px] border border-[#262626] py-4 flex flex-col justify-between">
+    <div className="bg-[#1A1A1A] cursor-pointer  transition-transform  hover:-translate-y-1 ease-in-out delay-150 duration-300  rounded-xl px-4 min-w-[170px] flex-shrink-0 w-[170px] border border-[#262626] py-4 flex flex-col justify-between">
       <div className="grid grid-cols-2 grid-rows-2 gap-1">
         {
         loading? Array.from({length:4}).map((_,index)=>(
